567e4a0f7804ca8269ba0cba7c863a3984e5dd8b
augustinekudiyirippil <augustinekudiyirippil@gmail.com>
2024-01-01 14:52:06 +0000

    Change made in Subtraction service

---
+++ SubtractionService/Controllers/SubController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SubtractionService.Controllers
+{
+
+    [Route("Subtraction-Service")]
+    public class SubController : Controller
+    {
+        [Route("Sub")]
+        public IActionResult Get([FromQuery] int firstNumber, [FromQuery] int secondNumber)
+        {
+            return Content((firstNumber - secondNumber).ToString());
+        }
+
+    }
+}
