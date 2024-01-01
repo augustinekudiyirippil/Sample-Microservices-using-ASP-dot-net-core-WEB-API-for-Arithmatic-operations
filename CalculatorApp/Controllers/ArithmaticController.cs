3d193fc43cc469a186eb32835f20e1eb57e17fd6
augustinekudiyirippil <augustinekudiyirippil@gmail.com>
2024-01-01 15:50:15 +0000

    Methos added in Arithamtic service to call add and subtract methods from Add and subtract services

---
+++ CalculatorApp/Controllers/ArithmaticController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApp.Controllers
+{
+
+    [Route("Calculate")]
+    public class ArithmaticController : Controller
+    {
+
+        [HttpGet]
+        [Route("Add")]
+        public IActionResult addNumbers([FromQuery] int num1, [FromQuery] int num2)
+        {
+            HttpClient client = new HttpClient();
+
+            string url = string.Format("http://localhost:5272/Addition-Service/Add?firstNumber={0}&secondNumber={1}", num1, num2);
+
+
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return new JsonResult("The result of addition is " + response.Content.ReadAsStringAsync().Result);
+
+
+            }
+
+            else
+            {
+                throw new Exception("Internal server error");
+            }
+
+
+
+
+
+
+
+        }
+
+
+
+
+        [HttpGet]
+        [Route("Sub")]
+        public IActionResult subtractNumbers([FromQuery] int num1, [FromQuery] int num2)
+        {
+            HttpClient client = new HttpClient();
+
+            string url = string.Format("http://localhost:5145/Subtraction-Service/Sub?firstNumber={0}&secondNumber={1}", num1, num2);
+
+
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return new JsonResult("The result of addition is " + response.Content.ReadAsStringAsync().Result);
+
+
+            }
+
+            else
+            {
+                throw new Exception("Internal server error");
+            }
+
+        }
+
+
+
+
+
+
+    }
+    }
