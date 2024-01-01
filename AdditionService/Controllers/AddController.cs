07b45620a897fc1c0c11cfffb5aaffb572d30284
augustinekudiyirippil <augustinekudiyirippil@gmail.com>
2024-01-01 14:44:48 +0000

    Add method created in Addition service

---
+++ AdditionService/Controllers/AddController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace AdditionService.Controllers
+{
+
+    [Route("Addition-Service")]
+    public class AddController : Controller
+    {
+        [Route("Add")]
+        public IActionResult Get([FromQuery] int firstNumber, [FromQuery] int secondNumber)
+        {
+            return Content((firstNumber + secondNumber).ToString());
+        }
+    }
+}
