cd7681c53f707bcc665e4d2243ce01046a0ff7c5
augustinekudiyirippil <augustinekudiyirippil@gmail.com>
2024-01-03 16:48:07 +0000

    Added service for Multiplication

---
+++ MultiplicationService/Controllers/MultiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiplicationService.Controllers
+{
+    [Route("Multiplication-Service")]
+    public class SubController : Controller
+    {
+        [Route("Multi")]
+        public IActionResult Get([FromQuery] int firstNumber, [FromQuery] int secondNumber)
+        {
+            return Content((firstNumber * secondNumber).ToString());
+        }
+
+    }
+}
