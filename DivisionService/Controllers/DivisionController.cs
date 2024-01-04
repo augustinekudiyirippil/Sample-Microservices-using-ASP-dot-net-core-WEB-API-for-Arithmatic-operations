37b5e88ad8d00956ad37fb2213a0a4e91212a158
augustinekudiyirippil <augustinekudiyirippil@gmail.com>
2024-01-04 08:34:55 +0000

    New service added for division

---
+++ DivisionService/Controllers/DivisionController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DivisionService.Controllers
+{
+    public class DivisionController : Controller
+    {
+        [Route("Division-Service")]
+        public class SubController : Controller
+        {
+            [Route("Division")]
+            public IActionResult Get([FromQuery] int firstNumber, [FromQuery] int secondNumber)
+            {
+                return Content((firstNumber / secondNumber).ToString());
+            }
+
+        }
+    }
+}
